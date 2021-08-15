b1e873210ee8104f8e8588f030aac8151b04d1b5
diffrxction <aryandiffract@gmail.com>
2021-08-15 23:16:03 +0530

    Updated structures.go to add pointer reference functions

--- structures.go
+++ structures.go
@@ -21,6 +21,11 @@ func (c car) kmh() float64 {
 func (c car) mph() float64 {
 	return float64(c.gasPedal) * (c.topSpeedKmph/kmh_multiple/usixteenbitmax)
 }
+// This is a pointer receiver that receives a pointer struct and modifies the value of the struct object (car in this
+// case). In brief, func below modifies the struct itself via pointer type.
+func (c *car) newTopSpeed(newSpeed float64) {
+	c.topSpeedKmph = newSpeed
+}
 
 func main() {
 	//aCar is a car structure and can be declared as shown below
@@ -28,8 +33,13 @@ func main() {
 	//aCar := car{gas_pedal: 16535, brake_pedal: 0, steering_wheel: 12562, top_speed_kmh: 225.0}
 	//aCar := car{gas_pedal_pos,brake_pedal_pos,steering_wheel_pos,top_speed_kmh}
 	aCar := car{22314, 0, 12562, 225.0}
+
 	fmt.Println("gas_pedal:", aCar.gasPedal, "brake_pedal:", aCar.brakeSpeed, "steering_wheel:", aCar.steeringWheel,
 		"top_speed:", aCar.topSpeedKmph)
 	fmt.Println(aCar.kmh())
 	fmt.Println(aCar.mph())
+	aCar.newTopSpeed(500)
+	fmt.Println("After changes made with the pointer receiver function: ")
+	fmt.Println("gas_pedal:", aCar.gasPedal, "brake_pedal:", aCar.brakeSpeed, "steering_wheel:", aCar.steeringWheel,
+		"top_speed:", aCar.topSpeedKmph)
 }
